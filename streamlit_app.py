ca80d7cb9b552b6059dfe59811f3905a9e12eb1c
Carlos Tapanta <a01657439@tec.mx>
2023-03-07 18:58:29 -0600

    beta

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,16 +10,25 @@ client = pymongo.MongoClient("mongodb+srv://equipo3:password3@cluster0.gkaurda.m
 db = client["test1"]
 
 studentProject = db["collection1"]
-projects = db["collection2"]
-cursor = projects.find()
-
 db2 = client["DireccionSS"]
 students = db2["students"]
+#projects = db2["projects"]
+partnerProjects = db2["partnerProjects"]
+projects_collection = db2["projects"]
 
-projects = []
-for document in cursor:
-    project_id = document["projectID"]
-    projects.append(project_id)    
+partner_id = "SF246543"
+partner_project_doc = partnerProjects.find_one({"partnerID": partner_id})
+
+projects = partner_project_doc["projectsIDs"]
+
+def overlap(start1, end1, start2, end2):
+    return start1 <= end2 and end1 >= start2
+
+if partner_project_doc:
+    # get the list of projectIDs for that partner
+    projects = partner_project_doc["projectsIDs"]
+else:
+    print(f"No document found for partner ID {partner_id}")
 
 with open('./config.yaml') as file:
     config = yaml.load(file, Loader=yaml.SafeLoader)
@@ -43,19 +52,50 @@ if authentication_status:
         if document:
             name = document["name"]
             major = document["major"]
-            st.text_input("Name", name)
-            st.text_input("Major", major)
+            hasInductionWeek = document["hasInductionWeek"]
+            if hasInductionWeek:
+                hasIW = "Sí"
+            else:
+                hasIW = "No"
+            st.text_input("Nombre", name)
+            st.text_input("Carrera", major)
+            st.text_input("Requisito Semana Inducción", hasIW)
         else:
             st.error("No se ha encontrado el estudiante")
     option = st.selectbox(
     'Elige el proyecto', projects)
     if st.button('Enviar'):
-        st.write('Enviado')
-        document = {
-        "studentID": studentID,
-        "projectID": option
-        }
-        studentProject.insert_one(document)
+        existing_record = studentProject.find_one({"studentID": studentID})
+        new_project = projects_collection.find_one({"projectID": option})
+        print(new_project)
+        print(existing_record)
+
+        if existing_record:
+            existing_project = projects_collection.find_one({"projectID": existing_record["projectID"]})
+            print(existing_project)
+            start1 = existing_project["startDate"]
+            end1 = existing_project["endDate"]
+            start2 = new_project["startDate"]
+            end2 = new_project["endDate"]
+            print(start1,end1,start2,end2)
+
+        if existing_record and overlap(start1, end1, start2, end2):
+            st.error("Este estudiante ya tiene un proyecto asignado durante este periodo")
+        elif not hasInductionWeek:
+            st.error("Este alumno no cumple con el requisito de semana de inducción")
+        elif new_project["quota"] <= 0:
+            st.error("El proyecto ha llegado al límite de estudiantes")
+        else:
+            st.write('Enviado')
+            document = {
+            "studentID": studentID,
+            "projectID": option
+            }
+            studentProject.insert_one(document)
+            result = projects_collection.update_one(
+                    {"projectID": option},
+                    {"$inc": {"quota": -1}}
+                )
 
 elif authentication_status == False:
     st.error('Username/password is incorrect')
